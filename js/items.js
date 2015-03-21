4f17fb65bff5eecb4c95f34691d54124ab46b90e
Nick Barna <nickbarna@knights.ucf.edu>
2015-03-21 13:47:02 -0400

    Formatting

--- js/items.js
+++ js/items.js
@@ -6,30 +6,25 @@ $(document).ready(function(){
 		
 		var dollarAmount = $('#dollar-amount').val();
 		
-		console.log(dollarAmount);
-		
 		$.ajax({
-                    url: "json/items-list.json",
-                  
-                    dataType: "json",
-                    success: function(data) {
+			url: "json/items-list.json",
+			dataType: "json",
+			success: function(data) {
                        
+			var itemsArray = data;
 						
-						var itemsArray = data;
+			for (var i=0; i < 6; i++) {
 						
-						
-						for (var i=0; i < 6; i++) {
-						
-							var itemvar = itemsArray.items[Math.floor(Math.random() * itemsArray.items.length)];
+				var itemvar = itemsArray.items[Math.floor(Math.random() * itemsArray.items.length)];
 							
-							
-							$('main').append('<div>You can buy ' + Math.floor(dollarAmount/itemvar.price) + ' ' +  itemvar.name + 's with your money!</div>');
-						}
+				$('main').append('<div>You can buy ' + Math.floor(dollarAmount/itemvar.price) + ' ' +  itemvar.name + 's with your money!</div>');
+			
+			}
 					
-					}
+			}
                
 						
-          });
+		});
 		
 	});
 
