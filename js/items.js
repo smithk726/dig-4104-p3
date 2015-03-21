61d78b25459256d5c7537055c22b7073699f5a84
Nick Barna <nickbarna@knights.ucf.edu>
2015-03-21 14:14:04 -0400

    Basic validation added

--- js/items.js
+++ js/items.js
@@ -6,48 +6,61 @@ $(document).ready(function(){
 	//on click function
 	$("#submit-button").click(function(e) {
 		
-		$('#items li').remove();
-		
 		//stop from submitting and refreshing
 		e.preventDefault();
 		
 		//get input value and save to variable
 		var dollarAmount = $('#dollar-amount').val();
 		
-		//get json data from file
-		$.ajax({
-			url: "json/items-list.json",
-			dataType: "json",
-			success: function(data) {
-            
-			//put json data into variable           
-			var itemsArray = data;
-			
-			//loop through array and get six random entries			
-			for (var i=0; i < 6; i++) {
-						
-				var itemvar = itemsArray.items[Math.floor(Math.random() * itemsArray.items.length)];
-				
-				var finalamt = Math.floor(dollarAmount/itemvar.price);
-				
-				//add list items			
-				$('#items').append('<li>You can buy ' + '<em>' + finalamt + ' ' +  itemvar.name + 's</em> with your money!</li>');
-			
-			}
-					
-			}
-               
-						
-		});
 		
-		$('#more-button').show();
+		//validation
+		if (dollarAmount) {
+		
+			//housekeeping
+			$('#items li').remove();
+			
+			//get json data from file
+			$.ajax({
+				url: "json/items-list.json",
+				dataType: "json",
+				success: function(data) {
+				
+				//put json data into variable           
+				var itemsArray = data;
+				
+				//loop through array and get six random entries			
+				for (var i=0; i < 6; i++) {
+							
+					var itemvar = itemsArray.items[Math.floor(Math.random() * itemsArray.items.length)];
+					
+					var finalamt = Math.floor(dollarAmount/itemvar.price);
+					
+					//add list items			
+					$('#items').append('<li>You can buy ' + '<em>' + finalamt + ' ' +  itemvar.name + 's</em> with your money!</li>');
+				
+				}
+						
+				}
+				   
+			});
+			
+			$('#more-button').show();	
+		
+		}  else {
+			
+			alert("YOU DIDN'T ENTER ANYTHING!");
+			
+			$('#more-button').hide();	
+			
+			//housekeeping
+			$('#items li').remove();
+		
+		}
 		
 	});
 
-	//on click function
+	//MORE function
 	$("#more-button").click(function(e) {
-		
-		$('#items li').remove();
 		
 		//stop from submitting and refreshing
 		e.preventDefault();
@@ -55,31 +68,50 @@ $(document).ready(function(){
 		//get input value and save to variable
 		var dollarAmount = $('#dollar-amount').val();
 		
-		//get json data from file
-		$.ajax({
-			url: "json/items-list.json",
-			dataType: "json",
-			success: function(data) {
-            
-			//put json data into variable           
-			var itemsArray = data;
+		
+		//validation
+		if (dollarAmount) {
+		
+			//housekeeping
+			$('#items li').remove();
 			
-			//loop through array and get six random entries			
-			for (var i=0; i < 6; i++) {
-						
-				var itemvar = itemsArray.items[Math.floor(Math.random() * itemsArray.items.length)];
+			//get json data from file
+			$.ajax({
+				url: "json/items-list.json",
+				dataType: "json",
+				success: function(data) {
 				
-				var finalamt = Math.floor(dollarAmount/itemvar.price);
+				//put json data into variable           
+				var itemsArray = data;
 				
-				//add list items			
-				$('#items').append('<li>You can buy ' + '<em>' + finalamt + ' ' +  itemvar.name + 's</em> with your money!</li>');
-			
-			}
+				//loop through array and get six random entries			
+				for (var i=0; i < 6; i++) {
+							
+					var itemvar = itemsArray.items[Math.floor(Math.random() * itemsArray.items.length)];
 					
-			}
-               
+					var finalamt = Math.floor(dollarAmount/itemvar.price);
+					
+					//add list items			
+					$('#items').append('<li>You can buy ' + '<em>' + finalamt + ' ' +  itemvar.name + 's</em> with your money!</li>');
+				
+				}
 						
-		});
+				}
+				   
+			});
+			
+			$('#more-button').show();	
+		
+		}  else {
+			
+			alert("YOU DIDN'T ENTER ANYTHING!");
+			
+			$('#more-button').hide();	
+			
+			//housekeeping
+			$('#items li').remove();
+		
+		}
 		
 	});
 	
