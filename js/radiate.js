e7897454086f9dec12b7efa1c7885ab672c3ae66
Nick Barna <nickbarna@knights.ucf.edu>
2015-04-26 21:04:32 -0400

    Smaller lines

--- js/radiate.js
+++ js/radiate.js
@@ -5,7 +5,7 @@ var p; // shortcut to reference prototypes
 // library properties:
 lib.properties = {
 	width: 550,
-	height: 400,
+	height: 263,
 	fps: 60,
 	color: "#FFFFFF",
 	manifest: []
@@ -2045,11 +2045,11 @@ p.nominalBounds = new cjs.Rectangle(4.1,-12,86.9,40.3);
 
 	// Layer 1
 	this.wave_one = new lib.Waves();
-	this.wave_one.setTransform(226.6,192.5,1,1,0,0,0,47.6,126.5);
+	this.wave_one.setTransform(281.6,93.5,0.619,0.619,0,0,0,47.6,126.5);
 
 	this.addChild(this.wave_one);
 }).prototype = p = new cjs.Container();
-p.nominalBounds = new cjs.Rectangle(458.3,254.2,86.7,43);
+p.nominalBounds = new cjs.Rectangle(529.8,139.3,53.6,26.7);
 
 })(lib = lib||{}, images = images||{}, createjs = createjs||{});
 var lib, images, createjs;
